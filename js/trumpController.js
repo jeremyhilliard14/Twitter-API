bef385bddecfe488caa1c5c22e38bbf654ef55fa
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-04-05 17:31:27 -0400

    added styling and started adding additional pages

---
+++ js/trumpController.js
@@ -0,0 +1,24 @@
+twitterApp.controller('trumpController', function($scope, $http, $routeParams, $interval){
+
+	var url = 'http://digitalcrafts.com/students/twitter/hashtag.php?hash=trump';
+	function trumpTweets(){
+		$http.get(url).success(function(data){
+			$scope.data=data.statuses;
+			for(i=0; i<$scope.data.length; i++){
+			var time = $scope.data[i].created_at;
+			var tweetTime = new Date(time);
+			$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
+			//console.log($scope.data[i].tweetSeconds);
+			console.log($scope.data[i].user);
+
+			$interval(function(){
+				for(i=0; i<$scope.data.length; i++){
+					var currentDate = new Date();
+					var currentTimeInSeconds = currentDate.getTime()/1000;
+					$scope.data[i].sinceTweeted = Math.floor(currentTimeInSeconds - $scope.data[i].tweetSeconds);
+				};
+			}, 1000);
+		}
+	}
+		//console.log(data.statuses);
+	});
--- js/twitterController.js
+++ js/twitterController.js
@@ -25,3 +25,31 @@ twitterApp.controller('twitterController', function($scope, $http, $routeParams,
 		//console.log(data.statuses);
 	});
 });
+
+// twitterApp.controller('twitterController', function($scope, $http, $routeParams, $interval){
+
+// 	var url = 'http://digitalcrafts.com/students/twitter/hashtag.php?hash=trump&secondhash=hillary';
+
+// 	function trumpTweets(){
+// 		$http.get(url).success(function(data){
+// 			$scope.data=data.statuses;
+// 			for(i=0; i<$scope.data.length; i++){
+// 			var time = $scope.data[i].created_at;
+// 			var tweetTime = new Date(time);
+// 			$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
+// 			//console.log($scope.data[i].tweetSeconds);
+// 			console.log($scope.data[i].user);
+
+// 			$interval(function(){
+// 				for(i=0; i<$scope.data.length; i++){
+// 					var currentDate = new Date();
+// 					var currentTimeInSeconds = currentDate.getTime()/1000;
+// 					$scope.data[i].sinceTweeted = Math.floor(currentTimeInSeconds - $scope.data[i].tweetSeconds);
+// 				};
+// 			}, 1000);
+// 		}
+		
+// 		//console.log(data.statuses);
+// 		});
+// 	}
+// });
