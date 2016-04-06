1922ff9da0cef4d005e2c161c54d86960115d991
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-04-06 08:19:17 -0400

    added pages for each candidate

--- js/twitterController.js
+++ js/twitterController.js
@@ -24,6 +24,24 @@ twitterApp.controller('twitterController', function($scope, $http, $routeParams,
 		
 		//console.log(data.statuses);
 	});
+	// function refreshTweets(){
+	// 	$http.get(url).success(function(data){
+	// 	$scope.data=data.statuses;
+	// 	for(i=0; i<$scope.data.length; i++){
+	// 	var time = $scope.data[i].created_at;
+	// 	var tweetTime = new Date(time);
+	// 	$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
+	// 	//console.log($scope.data[i].tweetSeconds);
+	// 	console.log($scope.data[i].user);
+
+	// 	$interval(function(){
+	// 		for(i=0; i<$scope.data.length; i++){
+	// 			var currentDate = new Date();
+	// 			var currentTimeInSeconds = currentDate.getTime()/1000;
+	// 			$scope.data[i].sinceTweeted = Math.floor(currentTimeInSeconds - $scope.data[i].tweetSeconds);
+	// 		};
+	// 	}, 1000);
+	// }
 });
 
 // twitterApp.controller('twitterController', function($scope, $http, $routeParams, $interval){
