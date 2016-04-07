d1893e18bb903bac531f7474809d493a9ac37562
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-04-07 08:17:43 -0400

    attempt at routes

--- js/mainController.js
+++ js/mainController.js
@@ -1,41 +1,42 @@
 var twitterApp = angular.module('twitterApp', ['ngRoute']);
 
 	twitterApp.config(function($routeProvider){
-		$routeProvider.when('/',{
-			templateUrl: 'pages/front.html',
+		$routeProvider.when('/#',{
+			templateUrl: 'index.html',
 			controller: 'twitterController'
 		});
-		$routeProvider.when('/trump-tweets',{
-			templateUrl: 'pages/trump-tweets.html',
-			controller: 'trumpController'
-		});
-		$routeProvider.when('/hillary-tweets',{
-			templateUrl: 'pages/hillary-tweets.html',
-			controller: 'hillaryController'
-		});
+		$routeProvider.when('/:candidate',{
+			controller: 'twitterController'
+			templateUrl: function($routeParams){
+			return 'candidate' + $routeParams.template + '.html';
+		};
+		// $routeProvider.when('/hillary-tweets',{
+		// 	templateUrl: 'pages/hillary-tweets.html',
+		// 	controller: 'hillaryController'
+		// });
 	});
 
-twitterApp.controller('twitterController', function($scope, $http, $routeParams, $interval){
+// twitterApp.controller('twitterController', function($scope, $http, $routeParams, $interval){
 
-	var url = 'http://digitalcrafts.com/students/twitter/hashtag.php?hash=trump&secondhash=hillary';
+// 	var url = 'http://digitalcrafts.com/students/twitter/hashtag.php?hash=trump&secondhash=hillary';
 
-	$http.get(url).success(function(data){
-		$scope.data=data.statuses;
-		for(i=0; i<$scope.data.length; i++){
-		var time = $scope.data[i].created_at;
-		var tweetTime = new Date(time);
-		$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
-		//console.log($scope.data[i].tweetSeconds);
-		console.log($scope.data[i].user);
+// 	$http.get(url).success(function(data){
+// 		$scope.data=data.statuses;
+// 		for(i=0; i<$scope.data.length; i++){
+// 		var time = $scope.data[i].created_at;
+// 		var tweetTime = new Date(time);
+// 		$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
+// 		//console.log($scope.data[i].tweetSeconds);
+// 		console.log($scope.data[i].user);
 
-		$interval(function(){
-			for(i=0; i<$scope.data.length; i++){
-				var currentDate = new Date();
-				var currentTimeInSeconds = currentDate.getTime()/1000;
-				$scope.data[i].sinceTweeted = Math.floor(currentTimeInSeconds - $scope.data[i].tweetSeconds);
-			};
-		}, 1000);
-	}
+// 		$interval(function(){
+// 			for(i=0; i<$scope.data.length; i++){
+// 				var currentDate = new Date();
+// 				var currentTimeInSeconds = currentDate.getTime()/1000;
+// 				$scope.data[i].sinceTweeted = Math.floor(currentTimeInSeconds - $scope.data[i].tweetSeconds);
+// 			};
+// 		}, 1000);
+// 	}
 		
-		//console.log(data.statuses);
-	});
+// 		//console.log(data.statuses);
+// 	});
