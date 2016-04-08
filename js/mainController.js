9d0dac083ed3cf0e0023fb633ab2e1d0967ab415
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-04-08 14:45:17 -0400

    changed routes info

--- js/mainController.js
+++ js/mainController.js
@@ -1,42 +1,33 @@
 var twitterApp = angular.module('twitterApp', ['ngRoute']);
 
+
+
 	twitterApp.config(function($routeProvider){
-		$routeProvider.when('/#',{
-			templateUrl: 'index.html',
+		console.log($routeProvider);
+		$routeProvider.when('/',{
+			templateUrl: 'candidate/front.html',
+			controller: 'mainController'
+		});
+		// $routeProvider.when('/:candidate',{
+		// 	controller: 'twitterController'
+		// 	templateUrl: function($routeParams){
+		// 	return 'candidate' + $routeParams.template + '.html';
+		// };
+		$routeProvider.when('/trump',{
+			templateUrl: 'candidate/trump.html',
 			controller: 'twitterController'
 		});
-		$routeProvider.when('/:candidate',{
+		$routeProvider.when('/hillary',{
+			templateUrl: 'candidate/hillary.html',
 			controller: 'twitterController'
-			templateUrl: function($routeParams){
-			return 'candidate' + $routeParams.template + '.html';
-		};
-		// $routeProvider.when('/hillary-tweets',{
-		// 	templateUrl: 'pages/hillary-tweets.html',
-		// 	controller: 'hillaryController'
-		// });
+		});
+		$routeProvider.otherwise({
+		redirectTo: '/',
+		templateUrl: 'candidate/front.html',
+		controller: 'mainController'
+		});
+
 	});
+	
 
-// twitterApp.controller('twitterController', function($scope, $http, $routeParams, $interval){
 
-// 	var url = 'http://digitalcrafts.com/students/twitter/hashtag.php?hash=trump&secondhash=hillary';
-
-// 	$http.get(url).success(function(data){
-// 		$scope.data=data.statuses;
-// 		for(i=0; i<$scope.data.length; i++){
-// 		var time = $scope.data[i].created_at;
-// 		var tweetTime = new Date(time);
-// 		$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
-// 		//console.log($scope.data[i].tweetSeconds);
-// 		console.log($scope.data[i].user);
-
-// 		$interval(function(){
-// 			for(i=0; i<$scope.data.length; i++){
-// 				var currentDate = new Date();
-// 				var currentTimeInSeconds = currentDate.getTime()/1000;
-// 				$scope.data[i].sinceTweeted = Math.floor(currentTimeInSeconds - $scope.data[i].tweetSeconds);
-// 			};
-// 		}, 1000);
-// 	}
-		
-// 		//console.log(data.statuses);
-// 	});
